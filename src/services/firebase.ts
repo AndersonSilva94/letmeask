9c254e3dc45d789e89b74acace74f84b37945d71
andy silva <andssilva.23@gmail.com>
2021-06-21 00:22:09 -0400

    feat: :rocket: set environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhfDHsL3D603vZUQrfFDHwMPICVckJiXI",
-  authDomain: "letmeask-afb92.firebaseapp.com",
-  databaseURL: "https://letmeask-afb92-default-rtdb.firebaseio.com",
-  projectId: "letmeask-afb92",
-  storageBucket: "letmeask-afb92.appspot.com",
-  messagingSenderId: "334044852531",
-  appId: "1:334044852531:web:b37cfcfddfaa0f0fae0e46"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
