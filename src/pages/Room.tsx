ecbc20d5df8e67aaf31192f4b93e937128495e26
andy silva <andssilva.23@gmail.com>
2021-06-23 14:10:53 -0400

    feat: :rocket: create new question

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,23 +1,51 @@
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 
 import '../styles/room.scss'
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
 }
 
 export function Room() {
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('')
+  const roomId = params.id;
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+    
+    if(newQuestion.trim() === '') return;
+
+    if(!user) throw new Error('You must be logged in'); // usar o toast aqui
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighLighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+  }
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode code={params.id}/>
+          <RoomCode code={roomId}/>
         </div>
       </header>
 
@@ -27,15 +55,29 @@ export function Room() {
           <span>4 perguntas</span>
         </div>
 
-        <form>
-          <textarea placeholder="O que você quer perguntar?" />
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
 
           <div className="form-footer">
-            <span>Para enviar uma pergunta, <button>faça seu login</button></span>
-            <Button type="submit">Enviar pergunta</ Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+            )}
+            <Button type="submit" disabled={!user}>Enviar pergunta</ Button>
           </div>
         </form>
       </main>
     </div>
   );
 }
+
+// ****************************
+// Lembrete: pesquisa a biblioteca react hot toast
